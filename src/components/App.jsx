43780f4c21e051dc4ef581ec77ca2576103bc85e
kate-zahorui <kate.zahorui@gmail.com>
2022-08-31 14:51:48 +0300

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import { NotFound } from './NotFound';
+import s from './App.module.css';
+
+// import Home from '../pages/Home/Home';
+// import Movies from '../pages/Movies/Movies';
+// import MovieDetails from '../pages/MovieDetails/MovieDetails';
+
+const LazyHome = lazy(() => import('../pages/Home/Home'));
+const LazyMovies = lazy(() => import('../pages/Movies/Movies'));
+const LazyMovieDetails = lazy(() =>
+  import('../pages/MovieDetails/MovieDetails')
+);
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+        <nav>
+          <NavLink to="/" className={s.navLink}>
+            Home
+          </NavLink>
+          <NavLink to="/movies" className={s.navLink}>
+            Movies
+          </NavLink>
+        </nav>
+      </header>
+      <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<LazyHome />} />
+            <Route path="/movies" element={<LazyMovies />} />
+            <Route path="/movies/:movieId/*" element={<LazyMovieDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </main>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import { App } from 'components/App';
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-05-movies-search">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
